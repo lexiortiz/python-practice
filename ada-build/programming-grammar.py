7376a61cd0913232640c31df3ef5a331c032d5f5
Lexi <alexisortiz473@gmail.com>
2023-01-13 21:09:49 -0500

    completed 01: var & assignment

---
+++ ada-build/programming-grammar.py
@@ -0,0 +1,123 @@
+# Ada Build 
+# Intro to Python 
+# Lesson 02 Assignment 
+
+# ** LEARNING GOALS **
+# Understand and identify different data types (string, integer, float).
+# Assign data to variables.
+# Use the print function.
+# Understand and use string concatenation and string interpolation.
+# Identify and debug simple syntax errors.
+
+# problem 1.1
+x = 5
+# what value does x now hold?
+5 # int
+
+# problem 1.2
+z = "Hello"
+# what value does z now hold?
+# Hello # str 
+
+# problem 1.3
+a = 5
+b = 3.2
+c = a + b
+# what values does c now hold?
+8.2 # float
+
+# problem 1.4
+var1 = "lawl"
+var2 = "brb"
+# what value does var2 now hold?
+# brb # str
+
+# problem 1.5
+e = 6 + 3
+# what values does e now hold?
+9 # int
+
+
+# problem 1.6
+f = 3.5
+f = f + 2
+# what value does f now hold?
+5.5 # float
+
+
+# problem 1.7
+poodle = 4
+pitbull = 3
+boxer = poodle + pitbull
+# what value does boxer now hold? 
+7
+
+# problem 1.8
+h = 5
+h = h + h
+# what values does h now hold?
+10
+
+# problem 1.9
+j = 1
+k = 2
+m = 3
+n = j + k + m
+# what value does n now hold?
+6
+
+# problem 1.10
+l = "moo"
+q = "quack"
+l  = q
+# what value does l now hold?
+# quack
+
+# problem 1.11
+r = "moo"
+s = "quack"
+t = "woof"
+r = t
+# what value does r now hold?
+# woof
+
+# problem 1.12
+u = 5
+u = u * 2
+u = u * 2
+u = u * 2
+# what value does u now hold?
+40
+
+# problem 1.13
+v = "b"
+z = "a"
+# what value does v now hold?
+b
+
+# problem 1.14
+aa = 3234
+bb = 2398
+cc = 0
+dd = (aa + bb) / cc
+# what value does dd now hold?
+# ZeroDivisionError 
+
+# problem 1.15
+yy = 7
+zz = yy % 2
+# what value does zz now hold?
+# Guess: 1
+print(zz)
+
+# problem 1.16
+ee = 12
+ff = ee % 4
+# what value does ff now hold?
+0
+
+# problem 1.17
+zz = 17
+hh = zz % 3
+# what value does hh now hold?
+2
