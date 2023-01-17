ec53587660a751f819ebce40d9cce0b063251cff
Lexi <alexisortiz473@gmail.com>
2023-01-17 16:41:11 -0500

    completed rock-paper-scissors project

---
+++ ada-build-practice/rock_paper_scissors.py
@@ -0,0 +1,40 @@
+# Rock, Paper, Scissors Project
+
+# Is player1 === player2?
+# Yes -> tie
+# No -> continue
+# Is player1 rock?
+    # Yes -> Is player2 scissors?
+        # Yes -> player1 won
+        # No ->  player2 won
+    # No -> continue
+# Is player1 paper?
+    # Yes -> Is player2 rock?
+        # Yes -> player1 won
+        # No ->  player2 won
+    # No -> continue
+# Is player1 scissors?
+    # Yes -> Is player2 paper?
+        # Yes -> player1 won
+        # No ->  player2 won
+
+player1 = 'rock'
+player2 = 'paper'
+
+if player1 == player2:
+    print('It\'s a tie!')
+elif player1 == "rock":
+    if player2 == "scissors":
+        print('Player 1 won!')
+    else:
+        print('Player 2 won!')
+elif player1 == "paper":
+    if player2 == "rock":
+        print('Player 1 won!')
+    else:
+        print('Player 2 won!')
+elif player1 == "scissors":
+    if player2 == "paper":
+        print('Player 1 won!')
+    else:
+         print('Player 2 won!')
