ac33818d436b685217fe3c1988777056f69b6f42
lexiortiz <alexisortiz473@gmail.com>
2024-02-08 13:46:56 -0500

    lesson 4 goals

---
+++ ada-build-practice/functions.py
@@ -0,0 +1,11 @@
+# Ada Build 
+# Intro to Python 
+# Lesson 04 Assignment 
+
+# ** LEARNING GOALS **
+# Use common built-in functions in Python.
+# Understand and use functions contained in modules.
+# Understand the utility of writing functions.
+# Write and use custom functions.
+# Understand and define the following terms: docstring, signature, arguments, parameters, return
+# Understand the results of an AssertionError.
