fbdd61906cf02e97f4e016f7f52cd19ba537b2cd
lexiortiz <alexisortiz473@gmail.com>
2024-02-13 09:51:17 -0500

    looping: lesson 5 goals

---
+++ ada-build-practice/looping.py
@@ -0,0 +1,10 @@
+# Ada Build 
+# Intro to Python 
+# Lesson 05 Assignment 
+
+# ** LEARNING GOALS **
+# Define the following terms: loops, iterator, iteration, iteration variable, counter-controlled, sentinel-controlled, loop table, for loop, range, while loop, blocks
+# Explain the purpose for iteration in creating programs.
+# Write loops in order to prevent code duplication and repetition.
+# Understand how a loop will execute the statements inside and what the resulting output will be.
+# Debug code with loops.
